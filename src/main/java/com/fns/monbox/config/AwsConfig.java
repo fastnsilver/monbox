fe79a92006f664ce6d90faab40ceaa9daae8e772
Chris Phillipson <fastnsilver@gmail.com>
2016-12-27 22:29:12 -0800

    Target region when running “off cloud” * change collection interval to once a minute

--- src/main/java/com/fns/monbox/config/AwsConfig.java
+++ src/main/java/com/fns/monbox/config/AwsConfig.java
@@ -1,10 +1,12 @@
 package com.fns.monbox.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.autoscaling.AmazonAutoScaling;
 import com.amazonaws.services.autoscaling.AmazonAutoScalingClient;
 import com.amazonaws.services.cloudwatch.AmazonCloudWatch;
@@ -20,19 +22,28 @@ public class AwsConfig {
 	@Configuration
 	protected static class LocalBootstrap {
 		
+		@Value("${cloud.aws.region.static}") 
+		private String region;
+		
 		@Bean
 		public AmazonEC2 ec2Client(AWSCredentialsProvider awsCredentialsProvider) {
-			return new AmazonEC2Client(awsCredentialsProvider);
+			AmazonEC2Client ec2Client = new AmazonEC2Client(awsCredentialsProvider);
+			ec2Client.configureRegion(Regions.fromName(region));
+			return ec2Client;
 		}
 		
 		@Bean
 		public AmazonCloudWatch cloudWatchClient(AWSCredentialsProvider awsCredentialsProvider) {
-			return new AmazonCloudWatchClient(awsCredentialsProvider);
+			AmazonCloudWatchClient cloudWatchClient = new AmazonCloudWatchClient(awsCredentialsProvider);
+			cloudWatchClient.configureRegion(Regions.fromName(region));
+			return cloudWatchClient;
 		}
 		
 		@Bean
 		public AmazonAutoScaling autoscalingClient(AWSCredentialsProvider awsCredentialsProvider) {
-			return new AmazonAutoScalingClient(awsCredentialsProvider);
+			AmazonAutoScalingClient autoScalingClient = new AmazonAutoScalingClient(awsCredentialsProvider);
+			autoScalingClient.configureRegion(Regions.fromName(region));
+			return autoScalingClient;
 		}
 		
 	}
